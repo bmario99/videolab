79b22d41db883d91cabcc5fd92e82b2b94bbc422
bmario99 <bonomario1999@gmail.com>
2024-04-06 16:53:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function playVideo() {
+    var video = document.getElementById("myVideo");
+    video.play();
+}
+
+function pauseVideo() {
+    var video = document.getElementById("myVideo");
+    video.pause();
+}
